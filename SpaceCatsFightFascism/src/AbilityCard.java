d2e06a4fe18e8c38a351c8addd1fc80d65ce7aa0
Alphabetical1 <47462795+Alphabetical1@users.noreply.github.com>
2022-11-15 18:07:29 -0400

    Filling out classes

--- SpaceCatsFightFascism/src/AbilityCard.java
+++ SpaceCatsFightFascism/src/AbilityCard.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 public class AbilityCard extends Card {
 
     public LinkedList<Card> GetActions() {
-        
+        return new LinkedList<>();
     }
     
 }
--- SpaceCatsFightFascism/src/Cat.java
+++ SpaceCatsFightFascism/src/Cat.java
@@ -1,3 +1,48 @@
 public class Cat {
-    
+
+    private String name;
+    private Planet homePlanet;
+    private AbilityCard ability;
+    private Planet planet;
+    private int scratchCount = 1; // I've set anything requiring a getter to be a private variable
+
+    public Cat(String inName, Planet inHomePlanet, AbilityCard inAbility) { // Constructor does not include planet and scratch count 
+                                                                            // since they are informed by the players actions.
+            name = inName;
+            homePlanet= inHomePlanet;
+            ability = inAbility;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Planet getHomePlanet() {
+        return homePlanet;
+    }
+
+    public AbilityCard getAbilityCard() {
+        return ability;
+    }
+
+    public Planet getPlanet() {
+        return planet;
+    }
+
+    public int getScratchCount() {
+        return scratchCount;
+    }
+
+    public void setScratchCount(int inScracthCount) {
+
+        if(inScracthCount > 4 || inScracthCount < 0) { // Throws exception if the incoming scratch count is out of bounds 0-4
+            throw new IllegalArgumentException("Scratch Count must be between 0-4");
+        }
+
+        scratchCount = inScracthCount;
+    }
+
+    public void setPLanet(Planet inPlanet){
+        planet = inPlanet;
+    }
 }
