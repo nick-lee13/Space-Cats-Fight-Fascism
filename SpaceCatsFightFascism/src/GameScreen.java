b65305d8ecc3e8efcbda3cba9081d344abfe87c4
Nick Lee <ntlee@mta.ca>
2022-11-14 21:02:27 -0400

    GUI commit

---
+++ SpaceCatsFightFascism/src/GameScreen.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class GameScreen extends Application {
+    @Override
+    public void start(Stage stage){
+        //start
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
