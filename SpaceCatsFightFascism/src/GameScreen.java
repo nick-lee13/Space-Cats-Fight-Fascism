b5cce80ff07ef3c4a444f3de907b43ff0730526c
Nick Lee <ntlee@mta.ca>
2022-11-15 20:28:56 -0400

    View commit

--- SpaceCatsFightFascism/src/GameScreen.java
+++ SpaceCatsFightFascism/src/GameScreen.java
@@ -1,16 +1,71 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class GameScreen extends Application {
     @Override
     public void start(Stage stage){
-        //start
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 1050, 600);
+        //Setting title to the scene 
+        stage.setTitle("Space Cats Fight Fascism"); 
+        try(InputStream is = Files.newInputStream(Paths.get("SpaceCatsFightFascism/res/game_bg.jpg"))){
+			ImageView img = new ImageView(new Image(is));
+			img.setFitWidth(1050);
+			img.setFitHeight(600);
+			root.getChildren().add(img);
+		}
+		catch(IOException e) {
+			System.out.println("Couldn't load image");
+		}
+        root.getChildren().addAll(drawRectangle(840,0,210,600), drawRectangle(0, 450, 1050, 150));
+        
+        //Draw Planets
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 4; j++){
+                root.getChildren().add(drawRectangle(60+(j*190), 105+(i*110), 150, 80));
+            }
+        }
+
+        //root.getChildren().add(0, root);
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    //Draw rectangle object to screen
+    private Rectangle drawRectangle(int x, int y, int width, int height){
+
+        Rectangle rect = new Rectangle(x,y,width,height);
+        rect.setFill(Color.LIGHTBLUE);
+
+        return rect;
+    }
+
+    private void drawPlanets(){
+        //drawPlanetCards()
+        //drawTokens()
+        //place cats
+
+    }
+
+    private void playerButtons(){
+        
     }
 }
