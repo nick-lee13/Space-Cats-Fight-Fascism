13210d685b421bd1cf11c5d52bfc60acc18e60c5
cjdennis634 <113532385+cjdennis634@users.noreply.github.com>
2022-11-16 10:35:26 -0400

    added an example for when i implement deck init

--- SpaceCatsFightFascism/src/GameState.java
+++ SpaceCatsFightFascism/src/GameState.java
@@ -99,6 +99,15 @@ public class GameState {
         //INITIALIZE ALL CARDS HERE, MAKE SURE TO GIVE EACH ONE AN ACTION IN THEIR cardAction VARIABLE
         //DONT FORGET THIS I STG
         //make sure to check the counts of each cards in the doc
+        
+        /* example below for future perusal:
+        ResistCard card = new Card();
+        card.playAction = new CardAction(){
+            public void action(){
+                card.owner.getCat().setScratchCount(card.owner.getCat().getScratchCount() - 2)
+                //(With appropriate cases if scratches < 2... etc...)
+            }
+        }*/
     }
 
     //initializes the galaxy news deck
