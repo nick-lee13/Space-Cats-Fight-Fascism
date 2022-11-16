6580468da4ef269c5da9f5e8bf6602161a26efb4
Kandrid <18233355+Kandrid@users.noreply.github.com>
2022-11-15 20:53:18 -0400

    Create GameController.java

---
+++ SpaceCatsFightFascism/src/GameController.java
@@ -0,0 +1,55 @@
+// GameController manages the main game logic and player behaviour functions
+
+class GameController {
+    GameState gameState;
+
+    // Runs the main logic for a single player's turn
+    public void runTurn() {
+
+    }
+
+    public void increaseFacism() {
+        gameState.setFascismScale(gameState.getFascismScale() + 1);
+    }
+
+    public int rollDice() {
+
+        return 0;
+    }
+
+    public Card drawResistCard() {
+        return null;
+    }
+
+    public Card drawGalaxyNewsCard() {
+        return null;
+    }
+
+    public void shuffleDecks() {
+
+    }
+
+    public void shufflePlanets() {
+
+    }
+
+    public void addScratch() {
+
+    }
+
+    public void travel(Planet planet) {
+
+    }
+
+    public void fight() {
+
+    }
+
+    public void restock() {
+
+    }
+
+    public void playCard() {
+
+    }
+}
