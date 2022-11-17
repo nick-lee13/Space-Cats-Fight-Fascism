b86865f5d030caf997fce9d683de1f89fec1fe8b
Andrew <creaghanandrew63@gmail.com>
2022-11-17 14:51:24 -0400

    added turn to Player and incrementturn method

--- SpaceCatsFightFascism/src/Model/Player.java
+++ SpaceCatsFightFascism/src/Model/Player.java
@@ -2,11 +2,17 @@ public class Player {
     Cat cat;
     Deck deck;
     int playerNum;
+    int turnCount;
 
     public Player(int playerNum, Cat cat) {
             this.cat = cat;
             deck = new Deck(this);
             this.playerNum = playerNum;
+            turnCount = 1;
+    }
+
+    public void incrementTurn(){ // Called when Player finishes turn to increase turn count
+        turnCount++;
     }
 
     public int getInput() {
